862ccecbc2fc6cac8b3dba59ac5b25919c6d8821
Gregory Todd Williams <greg@evilfunhouse.com>
2015-08-04 10:08:55 -0700

    Updated VERSION for 0.000_04 beta release.

--- lib/AtteanX/Store/MemoryTripleStore.pm
+++ lib/AtteanX/Store/MemoryTripleStore.pm
@@ -47,7 +47,7 @@ package AtteanX::Store::MemoryTripleStore {
 	use Scalar::Util qw(blessed);
 	
 	BEGIN {
-		our $VERSION	= '0.000_03';
+		our $VERSION	= '0.000_04';
 		XSLoader::load('AtteanX::Store::MemoryTripleStore', $VERSION);
 	}
 
